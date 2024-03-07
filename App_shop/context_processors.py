481d67fa48021ef2cafb630e73babaf8d61bdaaa
Zahid Hasan Milu <minions.milu889@gmail.com>
2024-03-07 14:25:16 +0600

    initial commit

--- App_shop/context_processors.py
+++ App_shop/context_processors.py
@@ -13,15 +13,14 @@ def categories(request):
 # Cart Count
 def cart_count(request):
     if request.user.is_authenticated:
-        cart_item = Cart.objects.filter(
-            user=request.user, purchased=False)
+        cart_items = Cart.objects.filter(user=request.user, purchased=False)
         order_queryset = Order.objects.filter(user=request.user, ordered=False)
         order = order_queryset.first()  # Fetch the first order if it exists
-        cart_count = cart_item.count()
+        cart_count = cart_items.count()
     else:
         cart_count = 0
-        cart_item = 0
-        order = 0
+        cart_items = []
+        order = []
 
-    context = {'cart_count': cart_count, 'cart_item': cart_item, 'order': order}
+    context = {'cart_count': cart_count, 'cart_items': cart_items, 'order': order}
     return context
