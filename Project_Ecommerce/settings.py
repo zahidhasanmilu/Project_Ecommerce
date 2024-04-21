ae50957b4ede09dba5e623a8ed0118aff7d21915
zahidhasanmilu <minions.milu889@gmail.com>
2024-04-22 05:58:00 +0600

    for production

--- Project_Ecommerce/settings.py
+++ Project_Ecommerce/settings.py
@@ -12,11 +12,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bq0kk!+tidsoovf!pfy@zig&uidb$_)#%*ozsj*r-6ah1y(2!6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = True
+# DEBUG = True
+# ALLOWED_HOSTS = []
 
-# ALLOWED_HOSTS = ['zahidhasanmilu.pythonanywhere.com']
-ALLOWED_HOSTS = []
+# For Production
+DEBUG = False
+ALLOWED_HOSTS = ['zahidhasanmilu.pythonanywhere.com']
+
 
 
 # Application definition
